9b32359f38ce5aacfd3060140b3af272b0f001ea
Denis Lazendić <denis.lazendic@promona.hr>
2016-09-04 21:06:19 +0200

    Add project files.

---
+++ SignalRProgressBarSimpleExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRProgressBarSimpleExample.Startup))]
+namespace SignalRProgressBarSimpleExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
